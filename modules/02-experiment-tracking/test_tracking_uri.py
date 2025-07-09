03e4b82c333b4902cfa45968be392d5795884c63
hello-isa <isabeljoyadriatico@gmail.com>
2025-07-09 16:08:39 +0800

    Remove unused Python scripts

--- modules/02-experiment-tracking/test_tracking_uri.py
+++
@@ -1,8 +0,0 @@
-import mlflow
-
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
-
-with mlflow.start_run():
-    mlflow.log_param("test_param", 123)
-    mlflow.log_metric("test_metric", 0.456)
-    print("Run logged to:", mlflow.get_tracking_uri())
